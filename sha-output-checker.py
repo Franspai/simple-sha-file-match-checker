6e55baf008db5aab2466469ee349b0647503e0bd
Franspai <franspaismail@gmail.com>
2022-08-24 18:22:30 +0200

    Main Commit

---
+++ sha-output-checker.py
@@ -0,0 +1,8 @@
+sha1 = input("Enter the first sha256/512 output: ")
+sha1 = sha1.lower()
+sha2 = input("Enter the second sha256/512 output: ")
+sha2 = sha2.lower()
+if sha1 == sha2:
+    print("You're not getting buttfucked")
+else:
+    print("You're getting buttfucked")
